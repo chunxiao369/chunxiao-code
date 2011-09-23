d65c05160dd677bf67bbf060b32c93a55c7cd369
xu.chunxiao <xu.chunxiao@embedway.com>
2011-09-23 16:49:08 +0800

    test address in kernel mode

--- driver-study/hello_world/hello_world.c
+++ driver-study/hello_world/hello_world.c
@@ -10,10 +10,18 @@ MODULE_LICENSE("Dual BSD/GPL");
 static int hello_init(void)
 {
     u64 time;
+    char *a = kmalloc(33, GFP_KERNEL);
     time = get_jiffies_64();
+    printk("%p.\n", &time);
+    printk("%p.\n", a);
+    printk("%p.\n", hello_init);
+    kfree(a);
+
+    /*
     printk("1. jiffies: %lu, HZ: %d, get_jiffies_64: %llu.\n", jiffies, HZ, time);
     printk(KERN_ALERT "Hello, world\n");
     printk("2. jiffies: %lu.\n", jiffies);
+    */
     return 0;
 }
 
