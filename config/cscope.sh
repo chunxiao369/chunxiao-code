d217e6ca33b5b7678181082a8d2756b694a3d188
xu.chunxiao <xu.chunxiao@embedway.com>
2011-11-23 12:45:21 +0800

    rm cscope temp files

--- config/cscope.sh
+++ config/cscope.sh
@@ -3,6 +3,7 @@ find `pwd` -name "*.[ch]" -type f > cscope.files
 ctags -R
 cscope -qb -i cscope.files
 
+rm -f cscope.files cscope.po.out cscope.in.out
 #find . \( -path "*.git*" -o  -path "*work.linux-2.2*" \) -prune -o -print > cscope.temp
 #grep "\.[ch]$" cscope.temp > cscope.files
 #rm -f cscope.temp
