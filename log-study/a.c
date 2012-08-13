ee7ab72d6757eb5a5f2d582daf2853a9efcef6e5
unknown <chunxiao@shht.(none)>
2012-08-13 21:17:07 +0800

    test log4c in multithread

--- log-study/a.c
+++ log-study/a.c
@@ -1,21 +1,50 @@
 
+#include <unistd.h>
 #include "log4c.h"
+#include "pthread.h"
 
-int main()
+log4c_category_t* mycat;
+static void *thread_func(void *arg)
 {
     int i = 0;
-    if (log4c_init()) {
-        printf("aaaaaaaaaaa");
-        return 0;
-    }
-    log4c_category_t* mycat = log4c_category_get("octeon_cat");
-    printf("mycat %p.\n", mycat);
+
     while (1) {
-        log4c_category_log(mycat, LOG4C_PRIORITY_DEBUG, "Hello World!, %d", i);
+        usleep(1);
+
+        log4c_category_log(mycat, LOG4C_PRIORITY_CRIT, "Hellocrit!, %d", i);
         i++;
         if (i >= 1000)
             break;
     }
+    return NULL;
+}
+
+int main()
+{
+    int i = 0;
+    pthread_t tid;
+
+    if (log4c_init()) {
+        printf("aaaaaaaaaaa");
+        return 0;
+    }
+    mycat = log4c_category_get("octeon_cat");
+    printf("mycat %p.\n", mycat);
+    //pthread_create(&tid, NULL, thread_func, NULL);
+
+    while (1) {
+        usleep(1);
+        log4c_category_log(mycat, LOG4C_PRIORITY_CRIT, "crit!, %d", i);
+        log4c_category_log(mycat, LOG4C_PRIORITY_ERROR, "error!, %d", i);
+        log4c_category_log(mycat, LOG4C_PRIORITY_WARN, "warn!, %d", i);
+        log4c_category_log(mycat, LOG4C_PRIORITY_INFO, "info!, %d", i);
+        log4c_category_log(mycat, LOG4C_PRIORITY_DEBUG, "debug!, %d", i);
+        log4c_category_log(mycat, LOG4C_PRIORITY_TRACE, "trace!, %d", i);
+        i++;
+        if (i >= 1)
+            break;
+    }
+    //pthread_join(tid, NULL);
     return 0;
 }
 
