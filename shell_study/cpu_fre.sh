0ee2fc988cb0add5e9859221f0b16a9bfbee6917
xu.chunxiao <xu.chunxiao@embedway.com>
2013-09-23 10:42:11 +0800

    chang cpu frequency shell

---
+++ shell_study/cpu_fre.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+case "$2" in
+1)
+        echo "Change to High performance.."
+        echo "performance" > /sys/devices/system/cpu/cpu$1/cpufreq/scaling_governor
+        ;;
+
+0)
+        echo "Change to normal performance.."
+        echo "conservative" > /sys/devices/system/cpu/cpu0$1/cpufreq/scaling_governor
+        ;;
+
+*)
+        printf "\nCPU Freq adjust tools v0.1 by xuchunxiao369@gmail.com\n"
+        printf "usage:\n"
+        printf "\t cpufreq.sh cpu_num 1 --> change to high performance mode\n"
+        printf "\t cpufreq.sh cpu_num 0 --> change to normal performance mode\n"
+        ;;
+
+esac
+
