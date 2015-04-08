c925881b487cf60c42d9ccd1d4c4c79af7fe4bc2
xuchunxiao <xuchunxiao369@embedway.com>
2015-04-08 13:43:12 +0800

    fix qemu command bug; update ipcommand

--- qemu-study/study.sh
+++ qemu-study/study.sh
@@ -4,6 +4,6 @@ qemu -m 128 -hda linux-0.2.img
 vncviewer 127.0.0.1:5900
 
 ---install---
-kvm 1024 -net nic,macaddr=00:11:11:EE:EE:EE -net tap,script=/etc/openvswitch/ovs-ifup,downscript=/etc/openvswitch/ovs-ifdown -drive file=centos.img -cdrom /home/chunxiao/Downloads/CentOS-7-x86_64-Minimal-1503.iso -boot c
+kvm -m 1024 -net nic,macaddr=00:11:11:EE:EE:EE -net tap,script=/etc/openvswitch/ovs-ifup,downscript=/etc/openvswitch/ovs-ifdown -drive file=centos.img -cdrom /home/chunxiao/Downloads/CentOS-7-x86_64-Minimal-1503.iso -boot d
 ----boot---
-kvm -m 1024 -net nic,macaddr=00:11:11:EE:EE:EE -net tap,script=/etc/openvswitch/ovs-ifup,downscript=/etc/openvswitch/ovs-ifdown -drive file=centos.img -boot d
+kvm -m 1024 -net nic,macaddr=00:11:11:EE:EE:EE -net tap,script=/etc/openvswitch/ovs-ifup,downscript=/etc/openvswitch/ovs-ifdown -drive file=centos.img -boot c
