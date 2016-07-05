c5cff773b1c7a215b1edea7695c0bb17fe000854
xu.chunxiao <xu.chunxiao@embedway.com>
2016-07-05 23:33:07 +0800

    R update

--- r-study/singer/singer.r
+++ r-study/singer/singer.r
@@ -14,4 +14,32 @@ plot(g,
     vertex.label.color='red',  #节点字体颜色
     edge.arrow.size=0.7)    #连线的箭头的大小
 #关闭图形设备，将缓冲区中的数据写入文件
+
+#关系图中某人的关系图
+g<-graph.data.frame(singer)
+gn<-graph.neighborhood(g, order=1)
+#周杰伦的关系图，所有与周杰伦有关系的人，以及这些人之间的关系
+plot(gn[[1]],layout=layout.fruchterman.reingold)
+#刘若英的关系图，所有与刘若英有关系的人，以及这些人之间的关系
+plot(gn[[2]],layout=layout.fruchterman.reingold)
+#凤凰传奇的关系图，所有与凤凰传奇有关系的人，以及这些人之间的关系
+plot(gn[[3]],layout=layout.fruchterman.reingold)
+
+#某个人的两层关系，三层以上的不会显示
+g<-graph.data.frame(singer)
+gn<-graph.neighborhood(g, order=2)
+plot(gn[[2]], layout=layout.fruchterman.reingold)
+
+#某两个人的关联图
+g<-graph.data.frame(singer)
+gn<-graph.neighborhood(g, order=1)
+plot(gn[[1]]+gn[[2]], layout=layout.fruchterman.reingold)
+
+#根据联系人的多少决定节点的大小和色彩，连线设成弧线
+source("http://michael.hahsler.net/SMU/ScientificCompR/code/map.R")
+#将连线设成弧线，数值越大弧线越弯
+E(g)$curved <- 0.2
+layout=layout.fruchterman.reingold
+plot(g, layout=layout, vertex.size=map(degree(g),c(1,20)), vertex.color=map(degree(g),c(1,20)))
+
 dev.off()
