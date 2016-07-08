8a87c6264f5b17f0295396b1e0b73db72f213e35
xu.chunxiao <xu.chunxiao@embedway.com>
2016-07-08 16:44:02 +0800

    node label

--- python-study/networkx/draw_graph.py
+++ python-study/networkx/draw_graph.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 import networkx as nx
 import matplotlib.pyplot as plt
 def draw_graph(graph):
@@ -14,9 +15,10 @@ def draw_graph(graph):
     # draw graph
     pos = nx.shell_layout(G)
     nx.draw(G, pos)
+    #nx.draw_networkx(G, pos, font_family='sans-serif')
     # show graph
     plt.show()
 
 # draw example
-graph = [(20, 21),(21, 22),(22, 23), (23, 24),(24, 25), (25, 20)]
+graph = [(20, 21),(21, 22),(22, 23), (23, 24),(24, 25), (25, 20), ('大小'.decode('utf8'), '多少'.decode('utf8'))]
 draw_graph(graph)
--- python-study/networkx/simple_path.py
+++ python-study/networkx/simple_path.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#coding:utf-8
 """
 Draw a graph with matplotlib.
 You must have matplotlib for this to work.
@@ -11,6 +12,7 @@ except:
 import networkx as nx
 
 G=nx.path_graph(8)
+G.add_edge('abcd', 'cxxu')
 nx.draw(G)
 plt.savefig("simple_path.png") # save as png
 plt.show() # display
