f1964c34211bef1e1e15e1240fa64a7bacb23ac3
lance <xuchunxiao369@gmail.com>
2017-09-14 21:51:13 +0800

    overcommit_memory

--- c-study/simple_test/malloc.c
+++ c-study/simple_test/malloc.c
@@ -1,5 +1,7 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -7,14 +9,31 @@
 /* only malloc not free 
  * os is running very well
  */
+#define MAX_SIZE (2 << 20)
 int main()
 {
-    char *p[1024];
+    char *p[1024] = {0};
     int i;
     printf("%d.\n",inet_addr("ip=202.255.255.255"));
-    for (i = 0; i < 1024; i++)
-        p[i] = malloc(1024 * 1024);
-    
+    for (i = 0; i < 1024; i++) {
+        usleep(10000);
+        p[i] = malloc(MAX_SIZE);
+        if (p[i] == NULL) {
+            printf("null, return, %d\n", i);
+            return 0;
+        }
+    }
+
+    for (i = 0; i < 1024; i++) {
+        memset(p[i], 0, MAX_SIZE);
+        memcpy(p[i], &i, 100);
+        usleep(10000);
+        if (p[i] == NULL) {
+            printf("null\n");
+        } else {
+            //free(p[i]);
+        }
+    }
     return 0;
 }
 
