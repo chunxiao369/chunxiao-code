50605759706ac4ba7f8381f3483e03151bf02e63
lance <xu.chunxiao369@163.com>
2022-08-02 16:59:21 +0800

    sh update

--- config/cscope.sh
+++ config/cscope.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 find `pwd` -name "*.[ch]" -type f > cscope.files
-ctags -R
+ctags -R -L cscope.files
 cscope -qb -i cscope.files
 
 #rm -f cscope.files cscope.po.out cscope.in.out
+#find . -path "*.svn*" -prune -o -print
 #find . \( -path "*.git*" -o  -path "*work.linux-2.2*" \) -prune -o -print > cscope.temp
 #grep "\.[ch]$" cscope.temp > cscope.files
 #rm -f cscope.temp
