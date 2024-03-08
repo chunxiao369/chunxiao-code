85db6560bb8c0cc8ccab2856945e1aa6baa685f1
chunxiao369 <xuchunxiao369@163.com>
2024-03-08 11:03:29 +0800

    cuda event example

--- c-study/openmpi/cuda_demo/Readme.md
+++ c-study/openmpi/cuda_demo/Readme.md
@@ -7,3 +7,11 @@ Hello, world from CPU!
 Device 0: NVIDIA A10, has SM Count: 72
 Device 1: NVIDIA A10, has SM Count: 72
 
+    /*
+    The function cudaEventSynchronize() blocks CPU execution
+    until the specified event is recorded.
+    The cudaEventElapsedTime() function
+    returns in the first argument the number of milliseconds time elapsed
+    between the recording of start and stop.
+    This value has a resolution of approximately one half microsecond.
+    */
--- c-study/openmpi/cuda_demo/memory.cu
+++ c-study/openmpi/cuda_demo/memory.cu
@@ -12,18 +12,39 @@
   } while (0)
 
 __global__ void kernel(int *pret) {
-    *pret = 42;
+    *pret = 666;
 }
+
+__global__ void just_print(int *gp) {
+    printf("from GPU print vlaue: %d\n", *gp);
+}
+
 
 int main() {
     int *pret;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
     CUDACHECK(cudaMalloc(&pret, sizeof(int)));
     kernel<<<1, 1>>>(pret);
 
     int ret;
     CUDACHECK(cudaMemcpy(&ret, pret, sizeof(int), cudaMemcpyDeviceToHost));
-    printf("result: %d\n", ret);
+    printf("from CPU print value: %d\n", ret);
 
+    cudaEventRecord(start);
+    /* GPU calc process*/
+    cudaEventRecord(stop);
+
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    ret += 222;
+    CUDACHECK(cudaMemcpy(pret, &ret, sizeof(int), cudaMemcpyHostToDevice));
+    just_print<<<1, 1>>>(pret);
+    //printf("result: %d\n", *pret);
     cudaFree(pret);
     return 0;
 }
