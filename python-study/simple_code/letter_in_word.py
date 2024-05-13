06106904f5b48ba60068dc5ab3f6f979d002d680
chunxiao369 <xuchunxiao369@163.com>
2024-05-13 09:46:25 +0800

    python code arrage

--- python-study/simple_code/letter_in_word.py
+++ python-study/simple_code/letter_in_word.py
@@ -7,7 +7,7 @@ def func(ssss):
     cnt=[0] * len(ssss)
     ret_cnt =[0,0,0,0,0] 
     for i in ssss:
-        print i, aaa[ord(i)-ord('A')]
+        print(i, aaa[ord(i)-ord('A')])
 
 #while len(ssss):
 func("BALLOONEEE")
